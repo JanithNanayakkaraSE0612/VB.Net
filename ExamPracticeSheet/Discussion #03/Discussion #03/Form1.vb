1bd83a2bcb428dbc696ce86952d112770184eee3
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-16 18:34:20 +0530

    ClearButton Function Complete

--- ExamPracticeSheet/Discussion #03/Discussion #03/Form1.vb
+++ ExamPracticeSheet/Discussion #03/Discussion #03/Form1.vb
@@ -10,4 +10,14 @@
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         Close()
     End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtBasicSalary.Text = ""
+        txtOH.Text = ""
+        txtSex.Text = ""
+        txtOR.Text = ""
+        txtAllow1.Text = ""
+        txtAllow2.Text = ""
+        txtNetSal.Text = ""
+    End Sub
 End Class
