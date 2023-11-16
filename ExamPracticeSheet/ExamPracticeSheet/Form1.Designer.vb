91c5e5077d260d230fbb8de6a0ef42e1d4e083a1
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-16 15:33:52 +0530

    Create and Descussion 3

--- ExamPracticeSheet/ExamPracticeSheet/Form1.Designer.vb
+++ ExamPracticeSheet/ExamPracticeSheet/Form1.Designer.vb
@@ -29,14 +29,14 @@ Partial Class Payment
         Label5 = New Label()
         Label6 = New Label()
         Label7 = New Label()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
-        TextBox4 = New TextBox()
-        TextBox5 = New TextBox()
-        TextBox6 = New TextBox()
-        Button1 = New Button()
-        Button2 = New Button()
+        txt1 = New TextBox()
+        txt2 = New TextBox()
+        txt3 = New TextBox()
+        txt4 = New TextBox()
+        txt5 = New TextBox()
+        txt6 = New TextBox()
+        btnSave = New Button()
+        btnExit = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -103,79 +103,79 @@ Partial Class Payment
         Label7.TabIndex = 6
         Label7.Text = "Bonus"
         ' 
-        ' TextBox1
+        ' txt1
         ' 
-        TextBox1.Location = New Point(250, 120)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(125, 27)
-        TextBox1.TabIndex = 7
+        txt1.Location = New Point(250, 120)
+        txt1.Name = "txt1"
+        txt1.Size = New Size(125, 27)
+        txt1.TabIndex = 7
         ' 
-        ' TextBox2
+        ' txt2
         ' 
-        TextBox2.Location = New Point(250, 162)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(125, 27)
-        TextBox2.TabIndex = 8
+        txt2.Location = New Point(250, 162)
+        txt2.Name = "txt2"
+        txt2.Size = New Size(125, 27)
+        txt2.TabIndex = 8
         ' 
-        ' TextBox3
+        ' txt3
         ' 
-        TextBox3.Location = New Point(250, 201)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(125, 27)
-        TextBox3.TabIndex = 9
+        txt3.Location = New Point(250, 201)
+        txt3.Name = "txt3"
+        txt3.Size = New Size(125, 27)
+        txt3.TabIndex = 9
         ' 
-        ' TextBox4
+        ' txt4
         ' 
-        TextBox4.Location = New Point(250, 243)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(125, 27)
-        TextBox4.TabIndex = 10
+        txt4.Location = New Point(250, 243)
+        txt4.Name = "txt4"
+        txt4.Size = New Size(125, 27)
+        txt4.TabIndex = 10
         ' 
-        ' TextBox5
+        ' txt5
         ' 
-        TextBox5.Location = New Point(250, 287)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(125, 27)
-        TextBox5.TabIndex = 11
+        txt5.Location = New Point(250, 287)
+        txt5.Name = "txt5"
+        txt5.Size = New Size(125, 27)
+        txt5.TabIndex = 11
         ' 
-        ' TextBox6
+        ' txt6
         ' 
-        TextBox6.Location = New Point(250, 327)
-        TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(125, 27)
-        TextBox6.TabIndex = 12
+        txt6.Location = New Point(250, 327)
+        txt6.Name = "txt6"
+        txt6.Size = New Size(125, 27)
+        txt6.TabIndex = 12
         ' 
-        ' Button1
+        ' btnSave
         ' 
-        Button1.Location = New Point(142, 404)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(94, 29)
-        Button1.TabIndex = 13
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        btnSave.Location = New Point(142, 404)
+        btnSave.Name = "btnSave"
+        btnSave.Size = New Size(94, 29)
+        btnSave.TabIndex = 13
+        btnSave.Text = "Clear"
+        btnSave.UseVisualStyleBackColor = True
         ' 
-        ' Button2
+        ' btnExit
         ' 
-        Button2.Location = New Point(259, 404)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(94, 29)
-        Button2.TabIndex = 14
-        Button2.Text = "Button2"
-        Button2.UseVisualStyleBackColor = True
+        btnExit.Location = New Point(259, 404)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(94, 29)
+        btnExit.TabIndex = 14
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
         ' 
         ' Payment
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(664, 463)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        Controls.Add(TextBox6)
-        Controls.Add(TextBox5)
-        Controls.Add(TextBox4)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
-        Controls.Add(TextBox1)
+        Controls.Add(btnExit)
+        Controls.Add(btnSave)
+        Controls.Add(txt6)
+        Controls.Add(txt5)
+        Controls.Add(txt4)
+        Controls.Add(txt3)
+        Controls.Add(txt2)
+        Controls.Add(txt1)
         Controls.Add(Label7)
         Controls.Add(Label6)
         Controls.Add(Label5)
@@ -196,12 +196,12 @@ Partial Class Payment
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents TextBox6 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents txt2 As TextBox
+    Friend WithEvents txt3 As TextBox
+    Friend WithEvents txt4 As TextBox
+    Friend WithEvents txt5 As TextBox
+    Friend WithEvents txt6 As TextBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnExit As Button
 End Class
