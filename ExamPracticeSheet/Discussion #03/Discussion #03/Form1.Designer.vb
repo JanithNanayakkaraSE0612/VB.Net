5acda32af7c5abb3d92d035705730b4a149af8d7
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-16 16:46:31 +0530

    label complete

--- ExamPracticeSheet/Discussion #03/Discussion #03/Form1.Designer.vb
+++ ExamPracticeSheet/Discussion #03/Discussion #03/Form1.Designer.vb
@@ -22,10 +22,223 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Button1.Location = New Point(526, 112)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Button2.Location = New Point(526, 215)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Button3.Location = New Point(526, 310)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox1.Location = New Point(277, 54)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 31)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox2.Location = New Point(277, 101)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 31)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox3.Location = New Point(277, 150)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 31)
+        TextBox3.TabIndex = 5
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox4.Location = New Point(277, 203)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 31)
+        TextBox4.TabIndex = 6
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox5.Location = New Point(277, 249)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 31)
+        TextBox5.TabIndex = 7
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox6.Location = New Point(277, 295)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 31)
+        TextBox6.TabIndex = 8
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        TextBox7.Location = New Point(277, 340)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(125, 31)
+        TextBox7.TabIndex = 9
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(109, 56)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(103, 25)
+        Label1.TabIndex = 10
+        Label1.Text = "Basic Salary"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(109, 108)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(87, 25)
+        Label2.TabIndex = 11
+        Label2.Text = "OT Hours"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(109, 157)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(81, 25)
+        Label3.TabIndex = 12
+        Label3.Text = "Sex(M/F)"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(109, 203)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(74, 25)
+        Label4.TabIndex = 13
+        Label4.Text = "OT Rate"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(109, 252)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(106, 25)
+        Label5.TabIndex = 14
+        Label5.Text = "Allownces 1"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label6.Location = New Point(109, 298)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(106, 25)
+        Label6.TabIndex = 15
+        Label6.Text = "Allownces 2"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Label7.Location = New Point(109, 347)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(92, 25)
+        Label7.TabIndex = 16
+        Label7.Text = "Net Salary"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
 End Class
