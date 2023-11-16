2ab4df65d54d213b6f994a81fad9d8ef92a3e5e4
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-16 14:45:20 +0530

    Added Text Box And Button

--- ExamPracticeSheet/ExamPracticeSheet/Form1.Designer.vb
+++ ExamPracticeSheet/ExamPracticeSheet/Form1.Designer.vb
@@ -22,16 +22,186 @@ Partial Class Payment
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(53, 44)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(300, 35)
+        Label1.TabIndex = 0
+        Label1.Text = "Ranasarani Service Center"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(68, 117)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(99, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Employee No"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(68, 162)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(49, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Name"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(68, 201)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(87, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "Basic Salary"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(68, 246)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(78, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Deduction"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(68, 287)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(77, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Net Salary"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(68, 334)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(49, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Bonus"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(250, 120)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 7
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(250, 162)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 8
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(250, 201)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 9
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(250, 243)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 10
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(250, 287)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 27)
+        TextBox5.TabIndex = 11
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(250, 327)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(125, 27)
+        TextBox6.TabIndex = 12
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(142, 404)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 13
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(259, 404)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 14
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
         ' 
         ' Payment
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(664, 463)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         Name = "Payment"
-        Text = "Payment"
+        Text = "Employee Details"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
