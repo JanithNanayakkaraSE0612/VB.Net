7eede60ce1b75f3b17997fde68307d6303f461f8
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-19 01:20:13 +0530

    FormSelection

--- ExamPracticeSheet/Department/Department/Department/Form1.Designer.vb
+++ ExamPracticeSheet/Department/Department/Department/Form1.Designer.vb
@@ -22,10 +22,164 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtNo = New TextBox()
+        txtBS = New TextBox()
+        TextBox3 = New TextBox()
+        GroupBox1 = New GroupBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton3 = New RadioButton()
+        btnCalculate = New Button()
+        btnExits = New Button()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(114, 37)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(63, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Emp No"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(114, 70)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(87, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Basic Salary"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(96, 296)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(53, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Label3"
+        ' 
+        ' txtNo
+        ' 
+        txtNo.Location = New Point(243, 30)
+        txtNo.Name = "txtNo"
+        txtNo.Size = New Size(125, 27)
+        txtNo.TabIndex = 3
+        ' 
+        ' txtBS
+        ' 
+        txtBS.Location = New Point(243, 70)
+        txtBS.Name = "txtBS"
+        txtBS.Size = New Size(125, 27)
+        txtBS.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(224, 296)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 5
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(32, 125)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(250, 125)
+        GroupBox1.TabIndex = 6
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Department"
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(32, 39)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(88, 24)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Acc (5%)"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(32, 65)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(103, 24)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Com (10%)"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(32, 95)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(94, 24)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Pro (15%)"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(495, 149)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(94, 29)
+        btnCalculate.TabIndex = 7
+        btnCalculate.Text = "Calculate"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' btnExits
+        ' 
+        btnExits.Location = New Point(495, 206)
+        btnExits.Name = "btnExits"
+        btnExits.Size = New Size(94, 29)
+        btnExits.TabIndex = 8
+        btnExits.Text = "Exit"
+        btnExits.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(620, 450)
+        Controls.Add(btnExits)
+        Controls.Add(btnCalculate)
+        Controls.Add(GroupBox1)
+        Controls.Add(TextBox3)
+        Controls.Add(txtBS)
+        Controls.Add(txtNo)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNo As TextBox
+    Friend WithEvents txtBS As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExits As Button
 End Class
