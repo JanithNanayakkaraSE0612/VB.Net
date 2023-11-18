e24fdd4729cd0a909a2f82c22bda7d6a66cff510
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-18 15:37:30 +0530

    add veriable textbox and Button

--- ExamPracticeSheet/ResultSheet/ResultSheet/ResultSheet/Form1.Designer.vb
+++ ExamPracticeSheet/ResultSheet/ResultSheet/ResultSheet/Form1.Designer.vb
@@ -30,17 +30,18 @@ Partial Class resultSheet
         Label6 = New Label()
         Label7 = New Label()
         Label8 = New Label()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
-        TextBox4 = New TextBox()
-        TextBox5 = New TextBox()
-        TextBox8 = New TextBox()
-        TextBox9 = New TextBox()
-        TextBox10 = New TextBox()
+        txtBio = New TextBox()
+        txtChe = New TextBox()
+        txtPhy = New TextBox()
+        txtEng = New TextBox()
+        txtTot = New TextBox()
+        txtAvg = New TextBox()
+        txtRes = New TextBox()
+        txtGrade = New TextBox()
         Button1 = New Button()
         Button2 = New Button()
         Button3 = New Button()
+        Button4 = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -115,61 +116,61 @@ Partial Class resultSheet
         Label8.TabIndex = 7
         Label8.Text = "Grade"
         ' 
-        ' TextBox1
+        ' txtBio
         ' 
-        TextBox1.Location = New Point(351, 57)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(125, 27)
-        TextBox1.TabIndex = 8
+        txtBio.Location = New Point(351, 57)
+        txtBio.Name = "txtBio"
+        txtBio.Size = New Size(125, 27)
+        txtBio.TabIndex = 8
         ' 
-        ' TextBox2
+        ' txtChe
         ' 
-        TextBox2.Location = New Point(351, 102)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(125, 27)
-        TextBox2.TabIndex = 9
+        txtChe.Location = New Point(351, 102)
+        txtChe.Name = "txtChe"
+        txtChe.Size = New Size(125, 27)
+        txtChe.TabIndex = 9
         ' 
-        ' TextBox3
+        ' txtPhy
         ' 
-        TextBox3.Location = New Point(354, 159)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(125, 27)
-        TextBox3.TabIndex = 10
+        txtPhy.Location = New Point(354, 159)
+        txtPhy.Name = "txtPhy"
+        txtPhy.Size = New Size(125, 27)
+        txtPhy.TabIndex = 10
         ' 
-        ' TextBox4
+        ' txtEng
         ' 
-        TextBox4.Location = New Point(354, 208)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(125, 27)
-        TextBox4.TabIndex = 11
+        txtEng.Location = New Point(354, 208)
+        txtEng.Name = "txtEng"
+        txtEng.Size = New Size(125, 27)
+        txtEng.TabIndex = 11
         ' 
-        ' TextBox5
+        ' txtTot
         ' 
-        TextBox5.Location = New Point(354, 257)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(125, 27)
-        TextBox5.TabIndex = 12
+        txtTot.Location = New Point(354, 257)
+        txtTot.Name = "txtTot"
+        txtTot.Size = New Size(125, 27)
+        txtTot.TabIndex = 12
         ' 
-        ' TextBox8
+        ' txtAvg
         ' 
-        TextBox8.Location = New Point(351, 304)
-        TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(125, 27)
-        TextBox8.TabIndex = 15
+        txtAvg.Location = New Point(351, 304)
+        txtAvg.Name = "txtAvg"
+        txtAvg.Size = New Size(125, 27)
+        txtAvg.TabIndex = 15
         ' 
-        ' TextBox9
+        ' txtRes
         ' 
-        TextBox9.Location = New Point(354, 348)
-        TextBox9.Name = "TextBox9"
-        TextBox9.Size = New Size(125, 27)
-        TextBox9.TabIndex = 16
+        txtRes.Location = New Point(354, 348)
+        txtRes.Name = "txtRes"
+        txtRes.Size = New Size(125, 27)
+        txtRes.TabIndex = 16
         ' 
-        ' TextBox10
+        ' txtGrade
         ' 
-        TextBox10.Location = New Point(351, 392)
-        TextBox10.Name = "TextBox10"
-        TextBox10.Size = New Size(125, 27)
-        TextBox10.TabIndex = 17
+        txtGrade.Location = New Point(351, 392)
+        txtGrade.Name = "txtGrade"
+        txtGrade.Size = New Size(125, 27)
+        txtGrade.TabIndex = 17
         ' 
         ' Button1
         ' 
@@ -177,7 +178,7 @@ Partial Class resultSheet
         Button1.Name = "Button1"
         Button1.Size = New Size(94, 29)
         Button1.TabIndex = 18
-        Button1.Text = "Button1"
+        Button1.Text = "Total"
         Button1.UseVisualStyleBackColor = True
         ' 
         ' Button2
@@ -186,7 +187,7 @@ Partial Class resultSheet
         Button2.Name = "Button2"
         Button2.Size = New Size(94, 29)
         Button2.TabIndex = 19
-        Button2.Text = "Button2"
+        Button2.Text = "Average"
         Button2.UseVisualStyleBackColor = True
         ' 
         ' Button3
@@ -195,25 +196,35 @@ Partial Class resultSheet
         Button3.Name = "Button3"
         Button3.Size = New Size(94, 29)
         Button3.TabIndex = 20
-        Button3.Text = "Button3"
+        Button3.Text = "Result"
         Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(612, 375)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(94, 29)
+        Button4.TabIndex = 21
+        Button4.Text = "Grade"
+        Button4.UseVisualStyleBackColor = True
         ' 
         ' resultSheet
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
         Controls.Add(Button3)
         Controls.Add(Button2)
         Controls.Add(Button1)
-        Controls.Add(TextBox10)
-        Controls.Add(TextBox9)
-        Controls.Add(TextBox8)
-        Controls.Add(TextBox5)
-        Controls.Add(TextBox4)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
-        Controls.Add(TextBox1)
+        Controls.Add(txtGrade)
+        Controls.Add(txtRes)
+        Controls.Add(txtAvg)
+        Controls.Add(txtTot)
+        Controls.Add(txtEng)
+        Controls.Add(txtPhy)
+        Controls.Add(txtChe)
+        Controls.Add(txtBio)
         Controls.Add(Label8)
         Controls.Add(Label7)
         Controls.Add(Label6)
@@ -236,15 +247,16 @@ Partial Class resultSheet
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents Label8 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents TextBox8 As TextBox
-    Friend WithEvents TextBox9 As TextBox
-    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents txtBio As TextBox
+    Friend WithEvents txtChe As TextBox
+    Friend WithEvents txtPhy As TextBox
+    Friend WithEvents txtEng As TextBox
+    Friend WithEvents txtTot As TextBox
+    Friend WithEvents txtAvg As TextBox
+    Friend WithEvents txtRes As TextBox
+    Friend WithEvents txtGrade As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 End Class
