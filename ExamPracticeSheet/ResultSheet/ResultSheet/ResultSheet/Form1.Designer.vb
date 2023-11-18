4a3d2eab4ca8fa2ebe7347ced2d741e3cc59b040
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-18 15:04:12 +0530

    Output

--- ExamPracticeSheet/ResultSheet/ResultSheet/ResultSheet/Form1.Designer.vb
+++ ExamPracticeSheet/ResultSheet/ResultSheet/ResultSheet/Form1.Designer.vb
@@ -22,16 +22,229 @@ Partial Class resultSheet
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
         SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(133, 60)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(60, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Biology"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(133, 109)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(74, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "Chemistry"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(133, 166)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(55, 20)
+        Label3.TabIndex = 2
+        Label3.Text = "Physics"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(133, 215)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(56, 20)
+        Label4.TabIndex = 3
+        Label4.Text = "English"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(133, 264)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(42, 20)
+        Label5.TabIndex = 4
+        Label5.Text = "Total"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(133, 311)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(64, 20)
+        Label6.TabIndex = 5
+        Label6.Text = "Average"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(133, 355)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(49, 20)
+        Label7.TabIndex = 6
+        Label7.Text = "Result"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(133, 399)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(49, 20)
+        Label8.TabIndex = 7
+        Label8.Text = "Grade"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(351, 57)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 8
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(351, 102)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 9
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(354, 159)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 10
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(354, 208)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(125, 27)
+        TextBox4.TabIndex = 11
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(354, 257)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(125, 27)
+        TextBox5.TabIndex = 12
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(351, 304)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(125, 27)
+        TextBox8.TabIndex = 15
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(354, 348)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(125, 27)
+        TextBox9.TabIndex = 16
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(351, 392)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(125, 27)
+        TextBox10.TabIndex = 17
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(612, 157)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 18
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(612, 231)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(94, 29)
+        Button2.TabIndex = 19
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(612, 311)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(94, 29)
+        Button3.TabIndex = 20
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
         ' 
         ' resultSheet
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         Name = "resultSheet"
         Text = "ResultSheet"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
