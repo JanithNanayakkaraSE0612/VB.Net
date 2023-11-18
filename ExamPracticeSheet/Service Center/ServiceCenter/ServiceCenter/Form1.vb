3dd6f6be535524e8de3cb24761c1c02257487859
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-18 17:56:29 +0530

    Complete This Service Center

--- ExamPracticeSheet/Service Center/ServiceCenter/ServiceCenter/Form1.vb
+++ ExamPracticeSheet/Service Center/ServiceCenter/ServiceCenter/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Dim No, Na, BS, Ded, NS, Bo As String
 
+
+    Private Sub btnExist_Click(sender As Object, e As EventArgs) Handles btnExist.Click
+        Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtNo.Text = ""
+        txtName.Text = ""
+        txtBS.Text = ""
+        txtDed.Text = ""
+        txtNS.Text = ""
+        txtB.Text = ""
+    End Sub
 End Class
